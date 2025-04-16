ad797ea9e91f5017c86c92434bdb638c29ce4749
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-04-16 13:43:35 +0200

    ajout des fetchs dans tmdb.js

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,10 +1,25 @@
-const API_KEY = "8c4b867188ee47a1d4e40854b27391ec";
+const API_KEY = "968470055c717530c2f304df9e76a976";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-export async function fetchPopularMovies() {
+export async function getPopularMovies(page = 1) {
   const response = await fetch(
-    `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=fr-FR&page=${page}`
   );
-  const data = await response.json();
-  return data.results;
+  if (!response.ok) throw new Error("Failed to fetch movies");
+  return response.json();
+}
+export async function getMovieDetails(movieId) {
+  const response = await fetch(
+    `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=fr-FR`
+  );
+  if (!response.ok) throw new Error("Failed to fetch movie details");
+  return response.json();
+}
+
+export async function getMovieReviews(movieId) {
+  const response = await fetch(
+    `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=fr-FR`
+  );
+  if (!response.ok) throw new Error("Failed to fetch movie reviews");
+  return response.json();
 }
